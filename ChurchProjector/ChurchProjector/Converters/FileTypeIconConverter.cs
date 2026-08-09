81c9d06f6fd54eb13d1a3951e297423afcd9a86d
helmut <helmut.bezolt@gmail.com>
2026-08-09 17:10:41 +0200

    fix add missing file

---
+++ ChurchProjector/ChurchProjector/Converters/FileTypeIconConverter.cs
@@ -0,0 +1,39 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using ChurchProjector.Classes;
+using System;
+using System.Globalization;
+
+namespace ChurchProjector.Converters;
+
+public class FileTypeIconConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        string? resourceKey = value switch
+        {
+            FileType.Image => "ImageFileIcon",
+            FileType.Movie => "MovieFileIcon",
+            FileType.Pdf => "PdfFileIcon",
+            FileType.Powerpoint => "PowerPointFileIcon",
+            FileType.Song => "LyricsFileIcon",
+            _ => null
+        };
+
+        if (resourceKey is null || Application.Current is not { } application)
+        {
+            return null;
+        }
+
+        return application.TryFindResource(resourceKey, out object? resource)
+            ? resource as StreamGeometry
+            : null;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
